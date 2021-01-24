8f101315125bb4fa0eec71f7f53bb053e5431ce3
Alex Russell <irrelevantmoniker@gmail.com>
2021-01-24 14:08:07 -0700

    completed assignment

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,34 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Creates a matrix that stores its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x   <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(newinv) inv <<- newinv
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
+}
+
+
+## Returns a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("retrieving cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setinv(inv)
+  inv
+}
